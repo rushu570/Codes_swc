587dcda9f927f7b335d4621406f256c7295c8e0e
RUSHANK JAIN <rushankjain833@gmail.com>
2023-05-31 00:06:05 +0530

    Update sum.cpp

--- BIT_0_BASED_INDEXING_RANGE_QUERY_AND_UPDATE/sum.cpp
+++ BIT_0_BASED_INDEXING_RANGE_QUERY_AND_UPDATE/sum.cpp
@@ -14,41 +14,50 @@ struct FenwickTree {
 
     FenwickTree(vector<int> const &a) : FenwickTree(a.size()) {
         for (size_t i = 0; i < a.size(); i++)
-            add(i, a[i]);
+            update(i, a[i]);
     }
 
-    int sum(int r) {
-        int ret = 0;
+    int query(int r) {
+        int sum = 0;
         for (; r >= 0; r = (r & (r + 1)) - 1)
-            ret += bit[r];
-        return ret;
+            sum += bit[r];
+        return sum;
     }
 
-    int sum(int l, int r) {
-        return sum(r) - sum(l - 1);
+    int query(int l, int r) {
+        return query(r) - (l > 0 ? query(l - 1) : 0);
     }
 
-    void add(int idx, int delta) {
+    void update(int idx, int value) {
         for (; idx < n; idx = idx | (idx + 1))
-            bit[idx] += delta;
+            bit[idx] += value;
     }
 
-    void rangeAdd(int l, int r, int delta) {
-        add(l, delta);
-        add(r + 1, -delta);
+    void rangeUpdate(int l, int r, int value) {
+        update(l, value);
+        if (r + 1 < n)
+            update(r + 1, -value);
     }
 };
 
 int main() {
     // Example usage
-    vector<int> arr = {1, 2, 3, 4, 5};
+    vector<int> arr = {1, 3, 2, 5, 4};
 
     int n = arr.size();
 
     FenwickTree tree(arr);
 
     // Perform a range query
-    int sum = tree.sum(1, 3);
-    cout << "Sum in range [1, 3]: " << sum << endl;
+    int sum_val = tree.query(1, 3);
+    cout << "Sum in range [1, 3]: " << sum_val << endl;
 
-    //
+    // Perform a range update
+    tree.rangeUpdate(2, 4, 2);
+
+    // Perform a range query after update
+    sum_val = tree.query(1, 3);
+    cout << "Sum in range [1, 3] after update: " << sum_val << endl;
+
+    return 0;
+}
