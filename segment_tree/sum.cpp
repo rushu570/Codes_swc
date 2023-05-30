2df37ac2d01e4ae48a83a8487f571ce634c399a6
RUSHANK JAIN <rushankjain833@gmail.com>
2023-05-31 00:41:45 +0530

    Update sum.cpp

--- segment_tree/sum.cpp
+++ segment_tree/sum.cpp
@@ -71,5 +71,10 @@ int main() {
     cout << "Sum in range [1, 3]: " << st.query(0, 0, st.n - 1, 1, 3) << endl;
     cout << "Sum in range [2, 4]: " << st.query(0, 0, st.n - 1, 2, 4) << endl;
 
-    // Update an element and perform range sum query
-    st.update(0, 0, st.n - 1, 2, 
+    // Update an element and perform range minimum query
+    st.update(0, 0, st.n - 1, 2, 1);
+    cout << "sum in range [1, 3] after update: " << st.query(0, 0, st.n - 1, 1, 3) << endl;
+
+    return 0;
+}
+ 
